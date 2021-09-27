f4d8aeeea3f6a8f665a11311eac02ece7a8707b8
yxxyn20 <dbtndus0527@gmail.com>
2021-09-27 14:26:37 +0900

    예제 문제 풀이 업로드

---
+++ ch03/Project1/ex4.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+void array_concat(int[], int, int[], int, int[]);
+
+int main(void)
+{
+	int n1 = 3;
+	int n2 = 5;
+
+	int A[] = {1,2,3};
+	int B[] = {4,5,6,7,8};
+	int C[20];
+
+	array_concat(A, n1, B, n2, C);
+
+	return 0;
+}
+
+void array_concat(int a[], int n1, int b[], int n2, int c[])
+{
+	int i;
+	for (i = 0; i < n1; i++) {
+		c[i] = a[i];
+	}
+	for (i = n1; i < n1 + n2; i++) {
+		c[i] = b[i - n1];
+	}
+
+	//Ãâ·Â
+	for (int i = 0; i < n1 + n2; i++) {
+		printf("%d ", c[i]);
+	}
+}
